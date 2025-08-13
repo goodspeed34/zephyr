514ef23a667015731f240302c5cf249bcd971cc6
William Goodspeed <goodspeed@anche.no>
2025-08-13 23:49:28 +0800

    boards: separate ws63flash from board.cmake
    Signed-off-by: William Goodspeed <goodspeed@anche.no>

---
+++ boards/common/ws63flash.board.cmake
@@ -0,0 +1,4 @@
+# SPDX-License-Identifier: Apache-2.0
+
+board_set_flasher_ifnset(ws63flash)
+board_finalize_runner_args(ws63flash)
--- boards/hisilicon/bearpi_pico_h3863/board.cmake
+++ boards/hisilicon/bearpi_pico_h3863/board.cmake
@@ -1,4 +1,4 @@
 # Copyright (c) 2025, Gong Zhile <gongzl@stu.hebust.edu.cn>
 # SPDX-License-Identifier: Apache-2.0
 
-board_set_flasher_ifnset(ws63flash)
+include(${ZEPHYR_BASE}/boards/common/ws63flash.board.cmake)
